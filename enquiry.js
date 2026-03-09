6beb2d73a01e1ae98b4d86e1ceec14b319a48f17
vishwaa30 <vishwajeet.3718@gmail.com>
2026-03-09 15:31:28 +0530

    Create enquiry.js

---
+++ enquiry.js
@@ -0,0 +1,20 @@
+function validateForm()
+{
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+
+if(name=="")
+{
+alert("Please enter your name");
+return false;
+}
+
+if(email=="")
+{
+alert("Please enter your email");
+return false;
+}
+
+alert("Thank you! Your enquiry has been submitted.");
+return true;
+}
